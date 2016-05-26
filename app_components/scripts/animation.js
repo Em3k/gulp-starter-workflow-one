b0e8a958967fb1ea58084ca331f2d2b59e3189fa
Em3k <emek.designer@gmail.com>
2016-05-26 13:44:07 +0100

    minor changes

--- app_components/scripts/animation.js
+++ app_components/scripts/animation.js
@@ -1,7 +1,7 @@
-var $ = require('jquery');
-var TweenMax       = require('../../node_modules/gsap/src/uncompressed/TweenMax.js');
+var $        = require('jquery');
+var TweenMax = require('../../node_modules/gsap/src/uncompressed/TweenMax.js');
 
 //var square = document.getElementById("square");
-var square = $("#square");
+var square   = $("#square");
 TweenLite.to(square, 5, {x:"500px", height:"150px"});
 console.log("animation.js");
